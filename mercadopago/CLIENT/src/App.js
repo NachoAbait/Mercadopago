6cfcef0a5a2fd00c8c08c4c388ddd07a110519f7
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-06-09 00:09:51 -0300

    tajetas con distintos productos, falta las rutas para c/u

--- mercadopago/CLIENT/src/App.js
+++ mercadopago/CLIENT/src/App.js
@@ -1,37 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { createOrder } from "./Actions/createOrder";
-import React, { useEffect } from "react";
-import { cleanOrder } from "./Actions/cleanOrden";
+import React from "react";
+import Card from "./Componentes/Cards/Card.jsx";
 
 function App() {
-  const dispatch = useDispatch();
-
-  const init_endpoint = useSelector((state) => state.init_endpoint);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    dispatch(cleanOrder());
-    console.log("este es el init-endpoint en handle");
-    console.log(init_endpoint);
-
-    dispatch(createOrder());
-    console.log("Estoy en el handle, se acaba de mandar la acción");
-    console.log(init_endpoint);
-  };
-
-  useEffect(() => {
-    if (init_endpoint.length) {
-      console.log("Redirigiendo a la URL:", init_endpoint);
-      window.location.href = init_endpoint;
-    }
-  }, [init_endpoint]);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={handleClick}>PAGAR</button>
-      </header>
+      <Card></Card>
     </div>
   );
 }
